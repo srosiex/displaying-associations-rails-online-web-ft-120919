0b05ad1c92fd397ab31e794b2204e3f356436c35
Sarah Henderson <sarahrose.henderson@gmail.com>
2020-10-23 14:38:48 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a = Author.create(name: "Stephen King", genre: "Horror", bio: "Old guy that writes scary books.")
+a.posts.create!(title: "First Post", description: "Here's the description of the blog post.")
+
+blog = Category.create!(name: "blog")
+
+blog = Post.first
